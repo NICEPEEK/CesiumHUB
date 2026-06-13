2138769a1a1541c284f43bb4f08d068dbfec775c
NICEPEEK <167718919+NICEPEEK@users.noreply.github.com>
2026-06-13 16:21:20 +0300

    a

--- server.js
+++ server.js
@@ -503,4 +503,4 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Сервер запущен: http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Сервер запущен: http://localhost:${PORT}`));
